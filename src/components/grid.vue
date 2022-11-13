3ec50a32c0be6f8fecfa313eaa5bd3e119912c49
Playboy-Jack <924969683@qq.com>
2022-11-13 20:15:19 +0800

    new file:   src/components/grid.vue 	deleted:    src/components/login.vue 	new file:   src/components/swipe.vue 	new file:   src/views/IndexView.vue

---
+++ src/components/grid.vue
@@ -0,0 +1,31 @@
+<script>
+import { createApp } from 'vue';
+import { Grid, GridItem } from 'vant';
+
+const app = createApp();
+app.use(Grid);
+app.use(GridItem);
+
+export default{
+
+}
+
+</script>
+
+<template>
+<van-grid>
+  <van-grid-item icon="photo-o" text="文字" />
+  <van-grid-item icon="photo-o" text="文字" />
+  <van-grid-item icon="photo-o" text="文字" />
+  <van-grid-item icon="photo-o" text="文字" />
+  <van-grid-item icon="photo-o" text="文字" />
+  <van-grid-item icon="photo-o" text="文字" />
+  <van-grid-item icon="photo-o" text="文字" />
+  <van-grid-item icon="photo-o" text="文字" />
+</van-grid>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,36 @@
+<script>
+import { ref } from 'vue';
+import searchVue from './components/search.vue';
+import tabbarVue from './components/tabbar.vue';
+import swipeVue from './components/swipe.vue';
+import gridVue from '../components/grid.vue';
+
+export default {
+  data() {
+    return {
+      visibleHello: true
+    }
+  },
+  components: {
+    searchVue,
+    tabbarVue,
+    swipeVue,
+    gridVue
+  },
+  setup() {
+    const active = ref(0);
+    return { active };
+  },
+};
+</script>
+
+<template>
+  <searchVue></searchVue>
+  <swipeVue></swipeVue>
+  <gridVue></gridVue>
+  <tabbarVue></tabbarVue>
+</template>
+
+<style scoped>
+
+</style>
