1c002be0dbcd41788e6aa7bb3828f06801d77b7d
Playboy-Jack <924969683@qq.com>
2022-11-24 21:45:56 +0800

    完成个人信息页面初步布局

---
+++ src/components/MineGrid.vue
@@ -0,0 +1,61 @@
+<script>
+import { createApp } from 'vue';
+import { Grid, GridItem } from 'vant';
+
+const app = createApp();
+app.use(Grid);
+app.use(GridItem);
+
+export default {
+  data(){
+    return{
+      texts :[
+        "我的创作",
+        "学习周报",
+        "我的收藏",
+        "浏览记录",
+        "我的学币",
+        "离线下载",
+        "错题集",
+        "帮助与反馈",
+      ],
+      photos:[
+      new URL("/src/assets/images/university.png", import.meta.url).href,
+      new URL("/src/assets/images/consult.png", import.meta.url).href,
+      new URL("/src/assets/images/improvement.png", import.meta.url).href,
+      new URL("/src/assets/images/Finance.png", import.meta.url).href,
+      new URL("/src/assets/images/Psychology.png", import.meta.url).href,
+      new URL("/src/assets/images/jurisprudence.png", import.meta.url).href,
+      new URL("/src/assets/images/Auditing.png", import.meta.url).href,
+      new URL("/src/assets/images/Law.png", import.meta.url).href,
+      ],
+    }
+  },
+  setup() {
+    
+  },
+}
+
+</script>
+
+<template>
+  <van-grid :column-num="4">
+  <van-grid-item v-for="(value, index) in texts" :icon="photos[index]" :text="texts[index]" />
+</van-grid>
+
+<van-grid square>
+      <van-grid-item 
+            v-for="(item,index) in gridtextlist" 
+            :key="index"
+            :text="item.text"
+            :icon="item.photo"
+      />
+      {{item}}
+    </van-grid>
+
+
+</template>
+
+<style scoped>
+
+</style>
--- src/views/IndexMine.vue
+++ src/views/IndexMine.vue
@@ -1,27 +1,91 @@
 <script>
+import MineGrid from '../components/MineGrid.vue'
+import { ref } from 'vue';
 export default{
-    
+  data(){
+    return {
+      setting: new URL("/src/assets/images/setting.png", import.meta.url).href,
+      saomiao:new URL("/src/assets/images/saomiao.png", import.meta.url).href,
+      night:new URL("/src/assets/images/night.png", import.meta.url).href,
+      message:new URL("/src/assets/images/message.png", import.meta.url).href,
+
+    }
+  },
+  setup(){
+    const checked = ref(true);
+    return { checked };
+  },
+  methods: {
+    getAssetsImages(name) {
+      return new URL(`/src/assets/images/${name}`, import.meta.url).href;
+    }
+  },
+  components:{
+    MineGrid
+  }
     
 }
 </script>
 
 <template>
 
+<div class="cover">
+  <van-icon :name="setting" class="set" size="25px"/>
+  <van-icon :name="saomiao" class="saomiao" size="20px"/>
+  <van-icon :name="night" class="night" size="30px"/>
+  <van-icon :name="message" class="message" size="25px"/>
 <van-image 
   round
-  class="cover"
+  fit="cover"
   width="100px"
   height="100px"
   src="https://img.yzcdn.cn/vant/cat.jpeg"
 />
+<div>
+  点击登录
+</div>
+</div>
+
+<!-- <van-config-provider theme="dark"><van-switch v-model="checked" />
+</van-config-provider> -->
+
+
+<mine-grid></mine-grid>
 
 </template>
 
 <style scoped>
 .cover{
+    position: relative;
     text-align: center;
-    padding: 100px;
-    background-color: aliceblue;
+    padding: 50px;
+    background-color: rgb(139, 108, 108);
     color: white;
 }
+.set{
+  position: absolute;
+  left: 330px;
+  bottom: 188px;
+  z-index: 2;
+}
+.saomiao{
+  position: absolute;
+  bottom: 190px;
+  left: 295px;
+}
+.night{
+  position: absolute;
+  bottom: 185px;
+  left: 250px;
+}
+.message{
+  position: absolute;
+  bottom: 188px;
+  left: 10px;
+}
+.van-theme-dark body {
+  text-color: #f5f5f5;
+  background-color: black;
+}
+
 </style>
